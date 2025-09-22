9c7a15d878a5a5a0165ef716f3d91fe2fdbab7b1
anthonyiscoding <142696453+anthonyiscoding@users.noreply.github.com>
2025-09-22 13:42:35 -0500

    remove erroneous text

--- app/customer/screens/quotes-detail.tsx
+++ app/customer/screens/quotes-detail.tsx
@@ -38,10 +38,10 @@ export function QuotesDetail() {
                             </CardTitle>
                         </CardHeader>
                         <CardContent className="space-y-6">
-                            <div>
+                            {/* <div>
                                 <h4 className="font-semibold mb-2">Services Included:</h4>
                                 <p className="text-gray-700">{selectedQuote.details}</p>
-                            </div>
+                            </div> */}
 
                             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                                 <div>
